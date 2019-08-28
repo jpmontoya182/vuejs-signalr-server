adc9f21ad0caee6531e8ef2e4450bec2f0124278
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2019-08-28 11:58:36 -0500

    firs.t commit, wit basic routes and CORS configuration on start up

---
+++ Models/Answer.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Answer{
+    public Guid Id { get; set; }
+    public Guid QuestionId { get; set; }
+    public string Body { get; set; }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Question {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int Score { get; set; }
+    public List<Answer> Answers { get; set; }  
+}
